c7f07724ef7c3d661ae5dabfe17e29a0bde63100
Uğur Akın <akinn.ugur@gmail.com>
2024-07-16 14:09:01 +0300

    GetOffers endpoint created and implemented

--- HotelBookAPI/Application/HotelBookAPI.Application/Features/AutoComplete/AutoCompleteResponse.cs
+++ HotelBookAPI/Application/HotelBookAPI.Application/Features/AutoComplete/AutoCompleteResponse.cs
@@ -1,4 +1,5 @@
-﻿using HotelBookAPI.Application.DTOs.Common.AutoComplete;
+﻿using HotelBookAPI.Application.DTOs.Common;
+using HotelBookAPI.Application.DTOs.Common.AutoComplete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace HotelBookAPI.Application.Features.AutoComplete
 {
     public class AutoCompleteResponse
     {
+        public Header Header{ get; set; }
         public ICollection<AutoCompleteItem> Items { get; set; }
 
 
