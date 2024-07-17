759559422d18b4f3f835b71946d17f99efcb0aa5
berkaycetin <berkay.cetin@sabanciuniv.edu>
2024-07-17 12:03:41 +0300

    add service response ok

--- HotelBookAPI/Application/HotelBookAPI.Application/Features/AddServices/AddServicesResponse.cs
+++ HotelBookAPI/Application/HotelBookAPI.Application/Features/AddServices/AddServicesResponse.cs
@@ -1,7 +1,10 @@
-﻿using HotelBookAPI.Application.Features.ProductInfo;
+﻿using HotelBookAPI.Application.DTOs.Common;
+using HotelBookAPI.Application.Features.ProductInfo;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Transactions;
@@ -84,15 +87,23 @@ namespace HotelBookAPI.Application.Features.AddServices
 
         public List<TravellersServices> Services { get; set; }
 
-        public int orderNumber { get; set; }
+        public int OrderNumber { get; set; }
 
-        public string birthDateFrom { get; set; }
+        public string BirthDateFrom { get; set; }
 
-        public string birthDateTo { get; set; }
+        public string BirthDateTo { get; set; }
 
-        public List<string> requiredFields { get; set; }
+        public List<string> RequiredFields { get; set; }
 
-        public List<AdditionalFields> additionalField { get; set; }
+        public List<Document> Documents { get; set; }
+
+        public int PassengerType { get; set; }
+
+        public List<AdditionalField> AdditionalFields { get; set; }
+
+        public List<InsertField> InsertFields { get; set; }
+
+        public int Status { get; set; }
 
     }
     public class availableTitle
@@ -157,36 +168,331 @@ namespace HotelBookAPI.Application.Features.AddServices
 
         public tPrice Price { get; set; }
 
-        public int passengerType { get; set; }
+        public int PassengerType { get; set; }
 
     }
     public class tPrice
     {
-        public int amount { get; set; }
+        public double Amount { get; set; }
 
-        public string currency { get; set; }
+        public string Currency { get; set; }
 
     }
 
-    public class AdditionalFields
+    public class Document { }
+
+    public class AdditionalField
     {
-        public string travellerTypeOrder { get; set; }
-        public string travellerUniqueID { get; set; }
-        public string tourVisio_TravellerId { get; set; }
-        public string paximum_TravellerId { get; set; }
-        public string birthDateFrom { get; set; }
-        public string birthDateTo { get; set; }
+        public string TravellerTypeOrder { get; set; }
+        public string TravellerUniqueID { get; set; }
+        public string TourVisio_TravellerId { get; set; }
+        public string Paximum_TravellerId { get; set; }
+        public string BirthDateFrom { get; set; }
+        public string BirthDateTo { get; set; }
 
     }
+    public class InsertField { }
 
     public class AddServicesReservationInfo
     {
+        public string BookingNumber { get; set; }
 
+        public AddServicesAgency Agency { get; set; }
+
+        public AddServicesAgencyUser AgencyUser { get; set; }
+
+        public string BeginDate { get; set; }
+        public string EndDate { get; set; }
+        public string Note { get; set; }
+
+        public PriceInfo SalePrice { get; set; }
+        public PriceInfo SupplementDiscount { get; set; }
+        public PriceInfo PassengerEB { get; set; }
+        public PriceInfo AgencyEB { get; set; }
+        public PriceInfo PassengerAmountToPay { get; set; }
+        public PriceInfo AgencyAmountToPay { get; set; }
+        public PriceInfo Discount { get; set; }
+        public PriceInfo AgencyBalance { get; set; }
+        public PriceInfo PassengerBalance { get; set; }
+        public CommissionInfo agencyCommission { get; set; }
+        public CommissionInfo brokerCommission { get; set; }
+        public CommissionInfo AgencySupplementCommission { get; set; }
+        public PriceInfo PromotionAmount { get; set; }
+        public PriceInfo PriceToPay { get; set; }
+        public PriceInfo AgencyPriceToPay { get; set; }
+        public PriceInfo PassengerPriceToPay { get; set; }
+        public PriceInfo TotalPrice { get; set; }
+        public int ReservationStatus { get; set; }
+        public int ConfirmationStatus { get; set; }
+        public int PaymentStatus { get; set; }
+
+        public List<Document> Documents { get; set; }
+        public List<Document> OtherDocuments { get; set; }
+
+        public ReservableInfos ReservableInfo { get; set; }
+        public int PaymentFrom { get; set; }
+
+        public CountryInfo DepartureCountry { get; set; }
+        public CountryInfo ArrivalCountry { get; set; }
+        public CityInfo DepartureCity { get; set; }
+        public CityInfo ArrivalCity { get; set; }
+        public string CreateDate { get; set; }
+        public AdditionalFields AdditionalFields { get; set; }
+        public string AdditionalCode1 { get; set; }
+        public string AdditionalCode2 { get; set; }
+        public string AdditionalCode3 { get; set; }
+        public string AdditionalCode4 { get; set; }
+        public int AgencyDiscount { get; set; }
+        public bool HasAvailablePromotionCode { get; set; }
+
+    }
+
+    public class AddServicesAgency
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public AddServiceAgencyCountry Country { get; set; }
+
+        public AddServiceAgencyAddress Address { get; set; }
+
+        public bool OwnAgency { get; set; }
+
+        public bool AceExport { get; set; }
+        
+    }
+
+    public class AddServiceAgencyAddress
+    {
+
+        public AddServiceAgencyAddressCountry Country { get; set; }
+
+        public AddServiceAgencyAddressCity City { get; set; }
+
+        public List<string> AddressLines { get; set; }
+
+        public string ZipCode { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+    }
+
+    public class AddServiceAgencyCountry
+    {
+        public string InternationalCode { get; set; }
+
+        public int Provider { get; set; }
+
+        public bool IsTopRegion { get; set; }
+
+    }
+    public class AddServiceAgencyAddressCountry
+    {
+        public string InternationalCode { get; set; }
+
+        public string Name { get; set; }
+
+        public int Provider { get; set; }
+
+        public bool IsTopRegion { get; set; }
+
+        public string Id { get; set; }
+
+    }
+    public class AddServiceAgencyAddressCity
+    {
+
+        public string InternationalCode { get; set; }
+
+        public string Name { get; set; }
+
+        public int Provider { get; set; }
+
+        public bool IsTopRegion { get; set; }
+
+        public string Id { get; set; }
+
+    }
+    public class AddServicesAgencyUser
+    {
+        public AddServicesAgencyUserOffice Office { get; set; }
+        public AddServicesAgencyUserOperator Operator { get; set; }
+        public AddServicesAgencyUserMarket Market { get; set; }
+        public AddServicesAgencyUserAgency Agency { get; set; }
+
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+    public class AddServicesAgencyUserOffice
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+    public class AddServicesAgencyUserOperator
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public bool AgencyCanDiscountCommission { get; set; }
+    }
+    public class AddServicesAgencyUserMarket
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+    public class AddServicesAgencyUserAgency
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public bool OwnAgency { get; set; }
+        public bool AceExport { get; set; }
+    }
+    public class PriceInfo
+    {
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+    }
+    public class CommissionInfo
+    {
+        public double Percent { get; set; }
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+
+    }
+    public class ReservableInfos
+    {
+        public bool Reservable { get; set; }
+    }
+    public class CountryInfo
+    {
+        public string Code { get; set; }
+        public string InternationalCode { get; set; }
+        public string Name { get; set; }
+        public int Type { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string ParentId { get; set; }
+        public string CountryId { get; set; }
+        public int Provider { get; set; }
+        public bool IsTopRegion { get; set; }
+        public string Id { get; set; }
+    }
+    public class CityInfo
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int Type { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string ParentId { get; set; }
+        public string CountryId { get; set; }
+        public int Provider { get; set; }
+        public bool IsTopRegion { get; set; }
+        public string Id { get; set; }
+    }
+    public class AdditionalFields
+    {
+        public string SmsLimit { get; set; }
+        public string PriceChanged { get; set; }
+        public string AllowSalePriceEdit { get; set; }
+        public string SendFlightSms { get; set; }
     }
     public class AddServicesServices
     {
+        public int OrderNumber { get; set; }
+        public string Note { get; set; }
+        public CountryInfo DepartureCountry { get; set; }
+        public CityInfo DepartureCity { get; set; }
+        public CountryInfo ArrivalCountry { get; set; }
+        public CityInfo ArrivalCity { get; set; }
+        public ServiceDetails ServiceDetails { get; set; }
+        public string PartnerServiceId { get; set; }
+        public bool IsMainService { get; set; }
+        public bool IsRefundable { get; set; }
+        public bool Bundle { get; set; }
+        public List<CancellationPolicy> CancellationPolicies { get; set; }
+        public List<Document> Documents { get; set; }
+        public string EncryptedServiceNumber { get; set; }
+        public List<PriceBreakDown> PriceBreakDowns { get; set; }
+        public int Commission { get; set; }
+        public ReservableInfos ReservableInfo { get; set; }
+        public int Unit { get; set; }
+        public List<ConditionalSpo> ConditionalSpos { get; set; }
+        public int ConfirmationStatus { get; set; }
+        public int ServiceStatus { get; set; }
+        public int ProductType { get; set; }
+        public bool CreateServiceTypeIfNotExists { get; set; }
+        public string Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string BeginDate { get; set; }
+        public string EndDate { get; set; }
+        public int Adult { get; set; }
+        public int Child { get; set; }
+        public int Infant { get; set; }
+        public PriceInfo Price { get; set; }
+        public bool IncludePackage { get; set; }
+        public bool Compulsory { get; set; }
+        public bool IsExtraService { get; set; }
+        public int Provider { get; set; }
+        public List<string> Travellers { get; set; }
+        public bool ThirdPartyRecord { get; set; }
+        public int RecordId { get; set; }
+        public ServiceAdditionalFields AdditionalFields { get; set; }
 
     }
+    public class ServiceDetails
+    {
+        public string ServiceId { get; set; }
+        public string Thumbnail { get; set; }
+        public HotelDetail HotelDetail { get; set; }
+        public int Night { get; set; }
+        public string Room { get; set; }
+        public string Board { get; set; }
+        public string Accom { get; set; }
+        public GeoLocation GeoLocation { get; set; }
+    }
+    public class HotelDetail
+    {
+        public HotelDetailAddress Address { get; set; }
+        public CityInfo TransferLocation { get; set; }
+        public int StopSaleGuaranteed { get; set; }
+        public int StopSaleStandart { get; set; }
+        public GeoLocation Geolocation { get; set; }
+        public CityInfo Location { get; set; }
+        public CountryInfo Country { get; set; }
+        public CityInfo City { get; set; }
+        public string Thumbnail { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class HotelDetailAddress
+    {
+        public List<string> AddressLines { get; set; }
+
+    }
+
+    public class GeoLocation
+    {
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+    }
+
+    public class CancellationPolicy
+    {
+        public string BeginDate { get; set; }
+        public string DueDate { get; set; }
+        public PriceInfo Price { get; set; }
+        public int Provider { get; set; }
+    }
+    public class ConditionalSpo { }
+    public class ServiceAdditionalFields
+    {
+        public string IsRefundable { get; set; }
+        public string ReservableInfo { get; set; }
+        public string IsEditable { get; set; }
+    }
+
     public class AddServicesPaymentDetail
     {
         public List<AddServicesPaymentPlan> PaymentPlan { get; set; }
@@ -200,7 +506,7 @@ namespace HotelBookAPI.Application.Features.AddServices
 
         public string DueDate { get; set; }
 
-        public AddServicesPaymentPlanPrice PaymentPlan { get; set; }
+        public AddServicesPaymentPlanPrice Price { get; set; }
 
         public bool PaymentStatus { get; set; }
 
@@ -223,7 +529,7 @@ namespace HotelBookAPI.Application.Features.AddServices
     
     
     
-    
+    public class PriceBreakDown { }
     public class AddServicesPaymentInfo {}
 
     public class AddServicesInvoices{}
