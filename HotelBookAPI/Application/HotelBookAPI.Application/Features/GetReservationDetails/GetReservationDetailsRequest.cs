be4022d75d3cf9dd686ae132701b6dcbeb525372
berkaycetin <berkay.cetin@sabanciuniv.edu>
2024-07-17 15:29:33 +0300

    GetReservationDetails response kaldı

--- HotelBookAPI/Application/HotelBookAPI.Application/Features/GetReservationDetails/GetReservationDetailsRequest.cs
+++ HotelBookAPI/Application/HotelBookAPI.Application/Features/GetReservationDetails/GetReservationDetailsRequest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HotelBookAPI.Application.Features.AddServices;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace HotelBookAPI.Application.Features.GetReservationDetails
 {
-    public class GetReservationDetailsRequest
+    public class GetReservationDetailsRequest : IRequest<GetReservationDetailsResponse>
     {
         public string ReservationNumber { get; set; }
     }
---
+++ HotelBookAPI/Presentation/HotelBookAPI.API/Controllers/GetReservationDetailsController.cs
@@ -0,0 +1,18 @@
+﻿using HotelBookAPI.Application.Features.GetReservationDetails;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetReservationDetailsController : BaseApiController
+    {
+        [HttpPost("GetReservationDetails")]
+        public async Task<IActionResult> GetReservationDetails(GetReservationDetailsRequest request)
+        {
+            var data = await Mediator.Send(request);
+            return Ok(data);
+        }
+
+    }
+}
